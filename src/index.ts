de34324af21978352c8989aec7c1fa9883b11602
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2025-04-05 02:15:32 -0600

    Refactor index.ts to import app from server module and remove unused routes

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,7 @@
-import express from "express";
-
-const app = express();
+import {app} from "@/server/server";
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-});
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.get("/about", (req, res) => {
-  res.send("About Page");
 });
