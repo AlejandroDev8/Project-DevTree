5f43835478af8a12084258fb73f83c5271aeee93
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2025-04-06 15:06:36 -0600

    Implement user registration logic in the router

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,7 +1,20 @@
+import { User } from "@/models/User";
 import { Router } from "express";
 
 export const router = Router();
 
-router.post('/auth/register', (req, res) => {
-  console.log(req.body);
+router.post('/auth/register', async (req, res) => {
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.json({
+      status: 200,
+      message: "User registered successfully"
+    });
+  } catch (error) {
+    res.json({
+      status: 500,
+      message: "Error registering user",
+    });
+  }
 })
